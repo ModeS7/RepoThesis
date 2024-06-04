2721bb6fac5d17d0ff54c2305fe33b95e96a40cf
amibekk <92865549+amibekk@users.noreply.github.com>
2024-06-04 08:49:18 +0200

    Update HD.py

--- Segmentation/HD.py
+++ Segmentation/HD.py
@@ -1,15 +1,18 @@
-from skimage import metrics
 
-def find_HD(y, y_pred):     
+from skimage import metrics
+# Function to find Hausdorff distance between the true segmentation mask y and the predicted segmentation mask y_pred
+def find_HD(y, y_pred):  # should have shapes [H, W]   
+    # label preditcted mask
     labeled_pred, count_pred = label(y_pred, return_num = True)
     pred_objects = regionprops(labeled_pred)
     pred_object_coord = [obj["coords"] for obj in pred_objects] 
 
-
+    # label ground truth
     labeled_gt, count_gt = label(y, return_num = True)
     true_objects = regionprops(labeled_gt)
     true_object_coord = [obj["coords"] for obj in true_objects] 
 
+    # If multiple true lesions, separate into different images. 1 lesion per image
     lesion_images_gt = []
     for elem in true_object_coord:
         lesion_image = np.zeros_like(labeled_gt)
@@ -18,7 +21,7 @@ def find_HD(y, y_pred):
 
         lesion_images_gt.append(lesion_image)
 
-
+    # If multiple predicted lesions, separate into different images. 1 lesion per image 
     lesion_images_pred = []
     for elem in pred_object_coord:
         lesion_image = np.zeros_like(labeled_gt)
@@ -31,6 +34,7 @@ def find_HD(y, y_pred):
     counter = 0
     for elem_pred in lesion_images_pred:
         for elem_gt in lesion_images_gt:
+            # If sum > 0 there is an overlap between the true and predicted lesion --> find hausdorff distance between the two
             if np.sum(elem_pred*elem_gt > 0):
                 HD = metrics.hausdorff_distance(elem_pred, elem_gt)
                 HD_list.append(HD)
