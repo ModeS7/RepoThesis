8eba5b443116d2e72e79cffd2b63903be01fcab6
amibekk <92865549+amibekk@users.noreply.github.com>
2024-06-04 08:33:11 +0200

    Update segmentation_training.py

--- Segmentation/segmentation_training.py
+++ Segmentation/segmentation_training.py
@@ -1,6 +1,18 @@
-'''Code for training the segmentation model. This code was adapted from the MONAI consortsium's tutorial for 3D spleen segmentation
-https://github.com/Project-MONAI/tutorials/blob/main/3d_segmentation/spleen_segmentation_3d.ipynb'''
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#############################################################################
+#This code was adapted from the MONAI consortsium's tutorial for 3D spleen segmentation
+#https://github.com/Project-MONAI/tutorials/blob/main/3d_segmentation/spleen_segmentation_3d.ipynb
 
+'''Code for training segmentation model'''
 from monai.data import Dataset, DataLoader, decollate_batch, CacheDataset
 from torch.utils.data import ConcatDataset
 from monai.transforms import Compose, LoadImage, ToTensor, ScaleIntensity, EnsureChannelFirst, Resize, CenterSpatialCrop, Activations,AsDiscrete, RandScaleIntensity, RandShiftIntensity, RandFlip, CropForeground, RandRotate90, RandSpatialCrop, RandRotate, RandAdjustContrast, RandHistogramShift, RandSpatialCrop, SpatialPad
